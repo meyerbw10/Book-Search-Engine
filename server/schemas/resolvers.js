6446ef0b48174cb032d9f2b7cf2ec7589e85d1ac
meyerbw10 <109110828+meyerbw10@users.noreply.github.com>
2022-10-06 16:01:48 -0400

    Adding Tutor Session, Added Schemas and Updated auth. stopped on error with resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,75 @@
+const { signToken } = require('../utils/auth')
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express')
+
+// WE STOPPED HREE 10/6
+
+const resolvers = {
+  Query: {
+    user: async (parent, args, context) => {
+      if(context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('You are not logged in!');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      // First we create the user
+      const user = await User.create({ username, email, password });
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if(context.user) {
+        const savedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } }
+        );
+
+        return savedUser;
+      }
+      throw new AuthenticationError('Could not save book!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if(context.user) {
+        const savedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } }
+        );
+
+        return savedUser;
+      }
+
+      throw new AuthenticationError('Could not delete book!');
+    },
+  }
+}
+module.exports = resolvers;
